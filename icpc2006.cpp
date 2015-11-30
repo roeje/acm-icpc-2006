7ffac9d5b10c993089e59846fd3dc36aefff04e5
Jesse Roe <jessedroe@gmail.com>
2015-11-30 13:04:50 +0800

    Updated initial commit

--- icpc2006.cpp
+++ icpc2006.cpp
@@ -1,7 +1,3 @@
-//
-// Created by Jesse on 11/22/2015.
-//
-
 #include <iostream>
 #include <sstream>
 #include <map>
@@ -21,8 +17,6 @@ pair <string, int> current_max;
 
 vector <pair <string, int>> results;
 
-string test_data = "This is a simple file of test data, which should\n not cause your program any trouble. Do note that\n it contains several punctuation characters. Of course, \n this is not really a problem, because such characters\n are treated in the same way as spaces.\n # \n Don't use contractions; it isn't nice.\n # \n# \n aBc def AbC def dfe ABC\n #123 \n#";
-
 void read_data(istream &input){
     string word;
 
@@ -40,35 +34,16 @@ void read_data(istream &input){
 }
 
 void replace(string &io){
-    for(int i = 0; i<io.size(); i++){
-        switch (io.at(i)){
-            case '.':
-                io[i] = ' ';
-                break;
-            case '\'':
-                io[i] = ' ';
-                break;
-            case ':':
-                io[i] = ' ';
-                break;
-            case ';':
-                io[i] = ' ';
-                break;
-            case ',':
-                io[i] = ' ';
-                break;
-        }
+    for(int i = 0;i<io.length();++i){
+        if(!(io[i]>='a' && io[i]<='z') && !(io[i]>='A' && io[i]<='Z')) io[i] = ' ';
+        else if(io[i]>='A' && io[i]<='Z') io[i] = 'a'+io[i]-'A';
     }
     split_str(io);
 }
 
-void get_biggest(map<string, int> &map) {
-    pair<string, int> res;
-}
-
 void split_str(string &str){
     stringstream ss(str);
-    //cout << "the stream: "<< ss<<end;
+
     while(ss.good()){
         string res = "";
         ss >> res;
@@ -84,10 +59,10 @@ void split_str(string &str){
 }
 
 int main(){
-    istringstream ss(test_data);
-    read_data(ss);
+    istream& data = cin;
+    read_data(data);
     for(int i = 0; i < results.size(); ++i){
-        cout << setw(4)<<results.at(i).second <<" "<<results.at(i).first<<endl;
+        cout << setw(4) << results.at(i).second << " " << results.at(i).first << endl;
     }
     return 0;
 }
