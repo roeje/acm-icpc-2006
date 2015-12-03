cab74daa443a622585cb0e24967512d38185122c
Jesse Roe <jessedroe@gmail.com>
2015-12-03 14:24:36 +0800

    Final version with comments.

--- icpc2006.cpp
+++ icpc2006.cpp
@@ -1,68 +1,97 @@
+//
+// Created by Jesse on 12/1/2015.
+//
+
 #include <iostream>
 #include <sstream>
 #include <map>
 #include <regex>
-#include <algorithm>
-#include <string>
 #include <iomanip>
 using namespace std;
 
-void read_data(istream &input);
-void replace(string &io);
-void split_str(string &str);
+void readData(istream& input);
+void updateCount(string& input);
+void filterText(string& input);
 
-map <string, int> my_map;
+pair <string, int> countForWord;
+vector <pair <string, int>> result;
+map <string, int> freqMap;
 
-pair <string, int> current_max;
 
-vector <pair <string, int>> results;
+void readData (istream &input) {
+    string currentWord;
 
-void read_data(istream &input){
-    string word;
-
-    while(input >> ws >> word){
-        if(word != "#"){
-            replace(word);
-        }else{
-            if(current_max.first != ""){
-                results.push_back(current_max);
+    /*Iterates through each word in the text input*/
+    /*Calls filter method for each word in paragraph*/
+    while (input >> ws >> currentWord) {
+        if (currentWord != "#"){
+            filterText(currentWord);
+        }
+        else {
+            if (countForWord.first != "") {
+                result.push_back(countForWord);
             }
-            current_max = make_pair("", 0);
-            my_map.clear();
+            countForWord = make_pair("", 0);
+            freqMap.clear();
+        }
+    }
+
+    /*Output final frequency count after map has been built*/
+    for(int it = 0; it < result.size(); ++it){
+        cout << setw(4) << result.at(it).second << " " << result.at(it).first << endl;
+    }
+}
+
+/*Handles the update of the frequency count for each unique word*/
+void updateCount(string& input){
+    stringstream data(input);
+
+    while (data.good()) {
+        string current = "";
+        data >> current;
+        if(current != ""){
+            transform(current.begin(), current.end(), current.begin(), ::tolower);
+            freqMap[current] += 1;
+            if(freqMap[current] > countForWord.second){
+                countForWord = make_pair(current, freqMap[current]);
+            }
         }
     }
 }
 
-void replace(string &io){
-    for(int i = 0;i<io.length();++i){
-        if(!(io[i]>='a' && io[i]<='z') && !(io[i]>='A' && io[i]<='Z')) io[i] = ' ';
-        else if(io[i]>='A' && io[i]<='Z') io[i] = 'a'+io[i]-'A';
-    }
-    split_str(io);
-}
-
-void split_str(string &str){
-    stringstream ss(str);
-
-    while(ss.good()){
-        string res = "";
-        ss >> res;
-        if(res != ""){
-            std::transform(res.begin(), res.end(), res.begin(), ::tolower);
-            my_map[res] += 1;
-            if(my_map[res] > current_max.second){
-                current_max = make_pair(res, my_map[res]);
-            }
+/*Checks each word for invalid characters and if found replaces with whitespace*/
+void filterText(string& input){
+    for(int it = 0; it < input.length(); ++it) {
+        if(!(input[it] >= 'a' && input[it] <= 'z') && !(input[it] >= 'A' && input[it] <= 'Z')) {
+            input[it] = ' ';
+        }
+        else if(input[it] >= 'A' && input[it] <= 'Z') {
+            input[it] = 'a' + input[it] - 'A';
         }
     }
-
+    updateCount(input);
 }
 
-int main(){
+/*Main that creates istream object passed to solving functions*/
+int main() {
+
+#ifndef ONLINE_JUDGE
+    string testData = "This is a simple file of test data, which should"
+            "not cause your program any trouble. Do note that"
+            "it contains several punctuation characters. Of course,"
+            "this is not really a problem, because such characters"
+            " are treated in the same way as spaces."
+            "#"
+            "Don't use contractions; it isn't nice."
+            "#"
+            "aBc def AbC def dfe ABC"
+            "#"
+            "123"
+            "#";
+
+    istringstream data (testData);
+#else
     istream& data = cin;
-    read_data(data);
-    for(int i = 0; i < results.size(); ++i){
-        cout << setw(4) << results.at(i).second << " " << results.at(i).first << endl;
-    }
-    return 0;
+#endif
+    readData(data);
 }
